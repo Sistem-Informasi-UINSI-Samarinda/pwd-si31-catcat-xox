397ed53f71c026f14f32e157818e5cad02054288
catcat_xox <wandisaputra4578@gmail.com>
2025-11-12 14:58:49 +0800

    praktikum 1

---
+++ pwd-si31/config/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$database = "pwd_2441919033_SI31";
+$username ="root";
+$password ="";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn){
+    die("koneksi gagal: ". mysqli_connect_error());
+}
+echo "koneksi berhasil";
+mysqli_close($conn);
+
+?>
