0dcaf3fbd319a12a855bcf299e7ab34c58500407
catcat_xox <wandisaputra4578@gmail.com>
2025-11-19 13:14:14 +0800

    first commit

--- pwd-si31/index.php
+++ pwd-si31/index.php
@@ -5,7 +5,7 @@
     $json = file_get_contents( 'https://gist.githubusercontent.com/ihgoyarp/9327fcd326756747e042bdc499ceef01/raw/235202678a029d072369806a9781b89f36707ab9/SI32_PWD_2025');
     $data = json_decode($json, TRUE);
 
-    var_dump($json);
+    // var_dump($json)
 ?>
 
     <section>
@@ -18,14 +18,14 @@
 
             <?php foreach($data as $key => $value): ?>
                 <tr>
-                    <td><?php echo $value['No'] ?></td>9
+                    <td><?php echo $value['No'] ?></td>
                     <td><?php echo $value['NIM'] ?></td>
                     <td><?php echo $value['Nama'] ?></td>
                 </tr>
             <?php endforeach; ?>
             
 
-        </table>
+            </table>
     </section>
 
 
